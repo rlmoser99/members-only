842d4480c91e550b153867d3700057376ed2150a
Rachel Moser <moser.rachel@gmail.com>
2020-12-11 12:58:47 -0600

    Installed gems and initial controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "posts#index"
+  
+  get 'posts/index'
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
