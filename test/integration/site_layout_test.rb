11f7918e802fb6d63112522824e958438d02d62d
Rachel Moser <moser.rachel@gmail.com>
2020-12-16 15:41:17 -0600

    add devise test helper

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,6 +3,11 @@ require 'pry'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  test "can get post index" do
+    get root_path
+    assert_response :success
+  end
+
   test "layout links when user is not signed in" do
     get root_path
     assert_template 'posts/index'
@@ -17,21 +22,36 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "p.post-author", count: 0
   end
   
+  test "user can make new post when signed in" do
+    get root_path
+    sign_in users(:madeline)
+    get new_post_path
+    assert_response :success
+  end
+
+  test "user can not make new post when not signed in" do
+    get root_path
+    get new_post_path
+    assert_response :redirect 
+  end
+
   test "layout links " do
     get root_path
     assert_template 'posts/index'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", new_user_session_path
     assert_select "a[href=?]", new_user_registration_path
+    # assert_select "p.post-content"
     assert_select "p.post-author", count: 0
-    user = users(:madeline)
-    sign_in_as(user)
+    # user = users(:madeline)
+    # sign_in_as(user)
+    sign_in users(:madeline)
     # assert_template 'posts/index'
     # assert_redirected_to root_path
     # follow_redirect!
-    # get root_path
-    assert_select "a[href=?]", root_path
-    assert_select "a[href=?]", new_user_session_path, count: 0
+    # get root_path 
+    # assert_select "a[href=?]", root_path
+    # assert_select "a[href=?]", new_user_session_path, count: 0
     # assert_select "a[href=?]", new_post_path
     # assert_select "a[href=?]", destroy_user_session_path
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,14 +9,5 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-  module SignInHelper
-    def sign_in_as(user)
-      post new_user_session_path(email: user.email, password: user.password)
-    end
-  end
-  
-  class ActionDispatch::IntegrationTest
-    include SignInHelper
-  end
+  include Devise::Test::IntegrationHelpers
 end
