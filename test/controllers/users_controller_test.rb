eb842a76b5f9eac38e8bc99829adb99072a9df55
Rachel Moser <moser.rachel@gmail.com>
2020-12-15 22:16:31 -0600

    update fixtures and sample test passing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:madeline)
+  end
+
+  test "should get new" do
+    get new_user_registration_path
+    assert_response :success
+  end
 end
